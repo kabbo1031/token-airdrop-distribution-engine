dfda78a20df1bd5f14ccb8f5ca8f03e4303c883e
Parmita <160781867+kabbo1031@users.noreply.github.com>
2026-01-18 14:14:47 +0600

    Create user.registry.js

---
+++ user.registry.js
@@ -0,0 +1,9 @@
+export function loadUsers() {
+  console.log("Loading users...");
+
+  return [
+    { address: "0xUSER001", activityScore: 80 },
+    { address: "0xUSER002", activityScore: 40 },
+    { address: "0xUSER003", activityScore: 95 }
+  ];
+}
